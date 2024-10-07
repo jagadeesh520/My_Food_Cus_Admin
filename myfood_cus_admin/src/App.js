512df240858d5512a7307a076d4476d8493ab117
Raychasree <85663703+Raychasree@users.noreply.github.com>
2024-10-07 16:27:51 +0530

    Add files via upload

---
+++ myfood_cus_admin/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import VendorForm from "./vendor_registration_form";
+import PlanSubscription from "./Plan_subscription"; 
+
+function App() {
+  return (
+    <div>
+      {}
+      {/* <VendorForm /> */}
+      
+      {}
+      <PlanSubscription />
+    </div>
+  );
+}
+
+export default App;
